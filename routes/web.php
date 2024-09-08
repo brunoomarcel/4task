94c0aaa247ed1decd32798b65ecf0239fa011862
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2024-09-08 14:24:26 -0300

    ajuste de rota da pasta public

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\TarefaController;
  * Adicione e modifique rotas conforme necessário
  */
 
+ Route::get('/', [TarefaController::class, 'index'])->name('tarefas.index');
+
  Route::resource('tarefas', TarefaController::class);  // Rotas para o recurso Tarefa
  Route::post('tarefas/{id}/concluir', [TarefaController::class, 'concluir'])->name('tarefas.concluir');
  Route::delete('tarefas/{id}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
